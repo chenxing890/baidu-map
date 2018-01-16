b4173b260a6e008a6748d6083fac88e1729be537
chen <272530596@qq.com>
2018-01-16 22:39:26 +0800

    添加定位包

---
+++ map/src/main/java/com/baidu/mapapi/SimpleDemo.java
@@ -0,0 +1,18 @@
+package com.baidu.mapapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 编译出src
+ * Created by chen on 2018/1/16.
+ */
+
+public class SimpleDemo extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
